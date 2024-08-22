63b77aca21bee370990097f67a82ff18933dd5aa
Mobinul Islam Mahi <mobinulislammahi@gmail.com>
2024-08-22 11:31:57 +0600

    new carreer

--- src/controller/admin/CareerController.js
+++ src/controller/admin/CareerController.js
@@ -1,7 +1,10 @@
 const CareerModel = require('./../../models/CareerModel')
+const UserModel = require('./../../models/UserModel')
 
 exports.CreateCareer = async(req, res) =>{
     try{
+        let user_id = req.headers.user_id
+        await UserModel.findOne({_id: user_id})
         let reqBody = req.body;
         const result = await CareerModel.create(reqBody)
         res.status(200).json({status: 'success', data: result})
---
+++ src/models/CareerModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const DataSchema = mongoose.Schema({
+    jobTitle: {type: String},
+    description: {type: String},
+    deadline: {type: String},
+    vacancy: {type: String},
+    experience: {type: String},
+    responsibilities: {type: String},
+    status: {type: String},
+    workplace: {type: String},
+    workingTime: {type: String},
+    education: {type: String},
+    salary: {type: String},
+    Benifits: {type: String},
+
+},{timestamps: true,versionKey:false})
+
+const CareerModel = mongoose.model('careers', DataSchema)
+module.exports = CareerModel
